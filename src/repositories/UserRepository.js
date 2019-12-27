eb318023e650949277497905942dc690190687dd
Robson <robson.silva.cr@gmail.com>
2019-12-27 09:29:19 -0300

    Project without debug

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,10 @@
+const users = ['Robson', 'Camilla', 'Reinaldo', 'Hilma']
+
+export function getUsers() {
+  // return users;
+  return []
+}
+
+export function addUser(user) {
+  users.push(user)
+}
